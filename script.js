a970adf5b7ecfbb463f86111f7e2f5f267914420
maggie charles <margarethinga0@gmail.com>
2024-08-30 04:37:05 -0900

    added menu

--- script.js
+++ script.js
@@ -90,3 +90,62 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+
+// -----------------------------------games----------------
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let slideContent = document.querySelectorAll("[explode]");
+
+// access buttons
+let nex = document.querySelector("[nexting]");
+let pre = document.querySelector("[preving]");
+
+var counterGames = 0;
+
+// code for after button
+nex.addEventListener("click", slideAfter);
+function slideAfter() {
+    slideContent[counterGames].style.animation = "nex1 0.5s ease-in forwards";
+    if (counterGames >= slideContent.length - 1) {
+        counterGames = 0;
+    } else {
+        counterGames++;
+    }
+    slideContent[counterGames].style.animation = "nex2 0.5s ease-in forwards";
+}
+
+// code for before button
+pre.addEventListener("click", slideBefore);
+function slideBefore() {
+    slideContent[counterGames].style.animation = "pre1 0.5s ease-in forwards";
+    if (counterGames == 0) {
+        counterGames = slideContent.length - 1;
+    } else {
+        counterGames--;
+    }
+    slideContent[counterGames].style.animation = "pre2 0.5s ease-in forwards";
+}
+
+
+function autoSliding() {
+    deletInterval = setInterval(timer,2000);
+    function timer() {
+        slideAfter();
+        indicators(); 
+    }
+}
+autoSliding() 
+
+
+// ---------------stop auto sliding when mouse is over-------------
+const container = document.querySelector(".explore-cont")
+container.addEventListener("mouseover", function() {
+    clearInterval(deletInterval);
+});
+// ---------------resume sliding when mouse is out-------------
+container.addEventListener("mouseout", autoSliding);
+
+
+})
+
